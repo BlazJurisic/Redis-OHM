55d7939bd65f55b861419f69509027aebf5c3721
Blaz Jurisic <jurisic.blaz@gmail.com>
2018-04-09 19:38:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const types = {
+    string: "string",
+    number: "number",
+    null: "null",
+    undefined: "undefined",
+    object: "object"
+}
+
+//This is just a sketch, its still under development
+//const client = Redis.Client...
+
+//FUNCTION STORE
+
+function _(hmsetId, object) {
+    const keys = object.keys()
+    keys.forEach(key => {
+        const property = object[key]
+        switch (typeof property) {
+            case types.string, types.number, types.null, types.undefined:
+                client.hset(hmsetId, key, property)
+                break
+            case types.object:
+                //TODO: if(Array.isArray(property) --> set flag to attach arr proto when reading
+                const nestedHmsetId = `${hmsetId}.${key}`
+                client.hset(hhmsetId, key, nestedHmsetId)
+                _(nestedHmSetId, property)
+                break
+            default:
+                client.hset(hmsetId, key, JSON.Stringify(property))
+        }
+    })
+}
+
+//FUNCTION READ
+
+function __(hmSetId, includeChildren) {
+    client.hgetAll(hmSetId, (err, reply) => {
+        if (err) console.log(err) //handle err
+        const keys = Object.keys(reply)
+        const ___ = keys.map(key => (
+            reply[key].includes(`${hmSetId}.`)
+                ? __(index, includeChildren)
+                : reply[key]
+        ))
+    })
+}
+
